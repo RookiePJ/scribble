50003cbbc69b107022b80b4c1dfecd36ac3d1089
norhh <nikhilparasaram@gmail.com>
2021-10-25 18:11:26 +0100

    Fix regex

--- src/instrumenter/annotations.ts
+++ src/instrumenter/annotations.ts
@@ -439,7 +439,7 @@ class AnnotationExtractor {
         const result: AnnotationMetaData[] = [];
 
         const rx =
-            /(\s*(\*|\/\/\/)(.*)\s*@custom:scribble\s*)?#?(if_succeeds|if_updated|if_assigned|invariant|assert|define\s*[a-zA-Z0-9_]*\s*\([^)]*\))/g;
+            /\s*(\*|\/\/\/)\s*((.*)@custom:scribble\s*)?#?(if_succeeds|if_updated|if_assigned|invariant|assert|define\s*[a-zA-Z0-9_]*\s*\([^)]*\))/g;
 
         let match = rx.exec(meta.text);
         while (match !== null) {
